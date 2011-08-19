ef82b0d97678c15c1dcb4e477b5108a7f5c2bf83
Magnus Klaar <magnus.klaar@gmail.com>
2011-08-19 17:13:36 +0200

    add function to join ranges

--- src/cowboy_sendfile_range.erl
+++ src/cowboy_sendfile_range.erl
@@ -18,13 +18,15 @@ get_range(Req) ->
 %% @end
 -spec parse_range(BinRange::binary(), ContentLength::pos_integer()) ->
           {Start::pos_integer(), End::pos_integer()}.
-parse_range(<<"bytes=", RangeSetBin/binary>> = BinRange, ContentLength) ->
-    hd([parse_range_spec(E, ContentLength) || E <- binary:split(RangeSetBin, [<<",">>])]);
+parse_range(<<"bytes=", RangeSetBin/binary>>, ContentLength) ->
+    RangeSetBins = binary:split(RangeSetBin, [<<",">>]),
+    RangeSet = [parse_range_spec(E, ContentLength) || E <- RangeSetBins],
+    join_ranges(RangeSet);
 parse_range(_Other, _ContentLength) ->
     error.
 
 %% @private Expect and parse a byte-range-spec.
--spec parse_range_spec(binary(), uint()) -> {Start::uint(), End::uint(), Length::uint()}.
+-spec parse_range_spec(binary(), uint()) -> {Start::uint(), End::uint()}.
 parse_range_spec(ElemBin, ContentLength) ->
     Range = case binary:split(ElemBin, [<<"-">>]) of
         [IStart, IEnd] -> {binary_to_integer(IStart), binary_to_integer(IEnd)};
@@ -35,27 +37,51 @@ parse_range_spec(ElemBin, ContentLength) ->
         {none, Length} when is_integer(Length) ->
             Start = ContentLength - Length,
             End = ContentLength - 1,
-            valid_range_spec(Start, End, Length, ContentLength);
+            valid_range_spec(Start, End, ContentLength);
         %% "N-" (From byte N to end of content)
         {Start, none} when is_integer(Start) ->
             End = ContentLength - 1,
-            Length = (End - Start) + 1,
-            valid_range_spec(Start, End, Length, ContentLength);
+            valid_range_spec(Start, End, ContentLength);
         %% "N-M" (From byte N to byte M)
         {Start, End} when is_integer(Start), is_integer(End) ->
-            Length = (End - Start) + 1,
-            valid_range_spec(Start, End, Length, ContentLength);
+            valid_range_spec(Start, End, ContentLength);
         _Other ->
             error
     end.
 
--spec valid_range_spec(uint(), uint(), uint(), uint()) -> {uint(), uint(), uint()}.
-valid_range_spec(Start, End, Length, ContentLength)
+%% @private Contstuct a valid byte-range. Return error on invalid input.
+-spec valid_range_spec(uint(), uint(), uint()) -> {uint(), uint()}.
+valid_range_spec(Start, End, ContentLength)
 when Start >= 0, Start < End, End < ContentLength ->
-    {Start, End, Length};
-valid_range_spec(_Start, _End, _Length, _ContentLength) ->
+    {Start, End};
+valid_range_spec(_Start, _End, _ContentLength) ->
     error.
 
+%% @private Join a set of byte-ranges.
+-spec join_ranges([{uint(), uint()}]) -> [{uint(), uint(), uint()}].
+join_ranges(Ranges) ->
+    case lists:sort(Ranges) of
+        [error|_] -> error;
+        [First|Sorted] -> join_ranges(Sorted, [First])
+    end.
+
+join_ranges([], [{OStart, OEnd}|OT]) ->
+    lists:reverse([{OStart, OEnd, (OEnd - OStart) + 1}|OT]);
+join_ranges([{IStart, IEnd}|IT], [{OStart, OEnd}|OT]=Output) ->
+    if  %% IStart and IEnd in output range
+        IStart >= OStart, IStart =< OEnd, IEnd =< OEnd ->
+            join_ranges(IT, Output);
+        %% IStart in output range and IEnd beyond output range.
+        IStart >= OStart, IStart =< OEnd, IEnd =< OEnd ->
+            join_ranges(IT, [{OStart, IEnd}|OT]);
+        %% IStart and IEnd beyond output range.
+        IStart > OEnd ->
+            ORange = {OStart, OEnd, (OEnd - OStart) + 1},
+            join_ranges(IT, [{IStart, IEnd},ORange|OT])
+    end.
+
+
+%% @private Convert a binary to an integr. Return error on invalid input.
 -spec binary_to_integer(binary()) -> non_neg_integer().
 binary_to_integer(<<>>) ->
     none;
@@ -80,10 +106,10 @@ binary_to_integer(Bin) ->
 
 rfc2615_examples_test_() ->
     P = fun(Bin) -> parse_range(Bin, 10000) end,
-    [?_assertEqual({0, 499, 500}, P(<<"bytes=0-499">>)),
-     ?_assertEqual({500, 999, 500}, P(<<"bytes=500-999">>)),
-     ?_assertEqual({9500,9999, 500}, P(<<"bytes=-500">>)),
-     ?_assertEqual({9500,9999, 500}, P(<<"bytes=9500-">>)),
+    [?_assertEqual([{0, 499, 500}], P(<<"bytes=0-499">>)),
+     ?_assertEqual([{500, 999, 500}], P(<<"bytes=500-999">>)),
+     ?_assertEqual([{9500,9999, 500}], P(<<"bytes=-500">>)),
+     ?_assertEqual([{9500,9999, 500}], P(<<"bytes=9500-">>)),
      ?_assertEqual(error, P(<<"notbytes=1-2">>)),
      ?_assertEqual(error, P(<<"bytes=10000-">>)),
      ?_assertEqual(error, P(<<"bytes=-">>)),

87537029beb12aa091dc7b22daa0ae3729ebc457
Magnus Klaar <magnus.klaar@gmail.com>
2011-08-20 13:26:09 +0200

    update CT suite to use lhttpc once

--- test/cs_SUITE.erl
+++ test/cs_SUITE.erl
@@ -31,7 +31,8 @@
 
 %% content test functions
 -export([ascii_one_chunk/1,
-         ascii_two_chunks/1]).
+         ascii_two_chunks/1,
+         ascii_hd_range/1]).
 
 all() ->
     [{group, static}, {group, content}].
@@ -47,17 +48,26 @@ groups() ->
         ]}] ++
     [{content, [], [
         ascii_one_chunk,
-        ascii_two_chunks
+        ascii_two_chunks,
+        ascii_hd_range
         ]}].
 
 init_per_suite(Config) ->
-    application:start(inets),
-    application:start(cowboy),
+    ok = application:start(inets),
+    ok = application:start(crypto),
+    ok = application:start(public_key),
+    ok = application:start(ssl),
+    ok = application:start(lhttpc),
+    ok = application:start(cowboy),
     Config.
 
 end_per_suite(_Config) ->
-    application:stop(cowboy),
-    application:stop(inets),
+    ok = application:stop(cowboy),
+    ok = application:stop(lhttpc),
+    ok = application:stop(ssl),
+    ok = application:stop(public_key),
+    ok = application:stop(crypto),
+    ok = application:stop(inets),
     ok.
 
 init_per_group(static, Config) ->
@@ -92,28 +102,39 @@ end_per_group(content, _Config) ->
     ok.
 
 %% content test function fixtures
+
 init_per_testcase(ascii_one_chunk=Name, Config) ->
-    File = filename:join(?config(priv_dir, Config), atom_to_list(Name)),
-    RFile0 = cs_rfile:make(512, <<"abcd">>),
-    {ok, RFile1} = cs_rfile:write_file(RFile0, File),
-    [{reference_file, RFile1}|Config];
+    init_test_file(Name, cs_rfile:make(512, <<"abcd">>), Config);
 
 init_per_testcase(ascii_two_chunks=Name, Config) ->
-    File = filename:join(?config(priv_dir, Config), atom_to_list(Name)),
-    RFile0 = cs_rfile:make(1024, <<"abcd">>),
-    {ok, RFile1} = cs_rfile:write_file(RFile0, File),
-    [{reference_file, RFile1}|Config];
+    init_test_file(Name, cs_rfile:make(1024, <<"abcd">>), Config);
+
+init_per_testcase(ascii_hd_range=Name, Config) ->
+    init_test_file(Name, cs_rfile:make(1024, <<"efg">>), Config);
 
 init_per_testcase(_Name, Config) ->
     Config.
 
 
 end_per_testcase(ascii_one_chunk, Config) ->
-    ok = cs_rfile:delete(?config(reference_file, Config));
+    end_test_file(ascii_one_chunk, Config);
+
+end_per_testcase(ascii_two_chunks, Config) ->
+    end_test_file(ascii_two_chunks, Config);
+
+end_per_testcase(ascii_hd_range, Config) ->
+    end_test_file(ascii_hd_range, Config);
 
 end_per_testcase(_Name, _Config) ->
     ok.
 
+init_test_file(Name, RFile0, Config) ->
+    File = filename:join(?config(priv_dir, Config), atom_to_list(Name)),
+    {ok, RFile1} = cs_rfile:write_file(RFile0, File), 
+    [{reference_file, RFile1}|Config].
+
+end_test_file(_Name, Config) ->
+    ok = cs_rfile:delete(?config(reference_file, Config)).
 
 %% static test functions
 
@@ -140,13 +161,11 @@ below_static_root_esc(Config) ->
 
 subdir_not_listed(Config) ->
     ?line(URL = build_url("/subdir", Config)),
-    ?line({ok, {Status, _Hdrs, _Body}} = httpc:request(URL)),
-    ?line({"HTTP/1.1", 403, "Forbidden"} = Status).
+    ?line({ok, {{"HTTP/1.1", 403, "Forbidden"}, _Hdrs, _Body}} = httpc:request(URL)).
 
 subdir_file_access(Config) ->
     ?line(URL = build_url("/subdir/subfile", Config)),
-    ?line({ok, {Status, _Hdrs, Body}} = httpc:request(URL)),
-    ?line({"HTTP/1.1", 200, "OK"} = Status),
+    ?line({ok, {{"HTTP/1.1", 200, "OK"}, _Hdrs, Body}} = httpc:request(URL)),
     %% TODO: It appears as if either cowboy_sendfile or httpc appends \n
     ?line("subfile-contents\n" = Body).
 
@@ -155,17 +174,22 @@ subdir_file_access(Config) ->
 
 ascii_one_chunk(Config) ->
     ?line(URL = build_url("/ascii_one_chunk", Config)),
-    ?line({ok, {Status, _Hdrs, Body}} = httpc:request(URL)),
-    ?line({"HTTP/1.1", 200, "OK"} = Status),
+    ?line({ok, {{"HTTP/1.1", 200, "OK"}, _Hdrs, Body}} = httpc:request(URL)),
     ?line(ReferenceData = cs_rfile:read_file(?config(reference_file, Config))),
     ?line(ReferenceData = list_to_binary(Body)).
 
 ascii_two_chunks(Config) ->
-    ?line(URL = build_url("/ascii_two_chunks", Config)),
-    ?line({ok, {Status, _Hdrs, Body}} = httpc:request(URL)),
-    ?line({"HTTP/1.1", 200, "OK"} = Status),
+    ?line({ok, {{200, "OK"}, _Hdrs, Body}} =
+        make_get("/ascii_two_chunks", [], Config)),
     ?line(ReferenceData = cs_rfile:read_file(?config(reference_file, Config))),
-    ?line(ReferenceData = list_to_binary(Body)).
+    ?line(ReferenceData = Body).
+
+ascii_hd_range(Config) ->
+    ?line({ok, {{200, "OK"}, _Hdrs, Body}} =
+        make_get("/ascii_hd_range", [{"Range", "bytes=0-499"}], Config)),
+    ?line(500 = byte_size(Body)).
+
+%% util functions
 
 static_dir(Which, Config) when Which =:= data_dir; Which =:= priv_dir ->
     Dir = ?config(Which, Config),
@@ -175,3 +199,7 @@ build_url(Path, Config) ->
     {scheme, Scheme} = lists:keyfind(scheme, 1, Config),
     {port, Port} = lists:keyfind(port, 1, Config),
     Scheme ++ "://localhost:" ++ integer_to_list(Port) ++ Path.
+
+make_get(Path, Headers, Config) ->
+    ?line(URL = build_url(Path, Config)),
+    lhttpc:request(URL, 'GET', Headers, infinity).
